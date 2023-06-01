57aa541998592bc702e4f3f8d0c24812c57e96ba
davidstanciu11 <stanciudavid169@gmail.com>
2023-06-01 13:33:43 +0300

    files organised

--- src/mainFiles/App.jsx
+++ src/mainFiles/App.jsx
@@ -1,12 +1,19 @@
-
+import Home from "../pages/Home";
+import Components from "../pages/Components";
+import { Route, Routes } from "react-router-dom";
+import Header from "../components/Header";
 
 function App() {
 
   return (
     <>
-      
+    <Header/>
+    <Routes>
+      <Route path="/home" element={<Home/>}/>
+      <Route path="/components" element={<Components/>}/>
+    </Routes>
     </>
   )
 }
 
-export default App
+export default App;
