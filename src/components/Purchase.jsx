27b21765771c9c0de07942005939ca7f964715fc
davidstanciu11 <stanciudavid169@gmail.com>
2023-06-15 12:14:27 +0300

    deployment issue banner

--- src/components/Purchase.jsx
+++ src/components/Purchase.jsx
@@ -1,10 +1,11 @@
 import { Button } from "@mui/material";
 import "./Purchase.style.css";
+import AdvertismentBanner from "../assets/advertisment.png";
 
 export default function Purchase() {
   return (
     <>
-      <section className="purchase_section">
+      <section className="purchase_section" style={{background: `url(${AdvertismentBanner})`}}>
         <div className="purchase_flex">
           <h2 className="purchase_title">
             <span className="purchase_start">Start Now</span> <br /> Create Your <br /> Website Today
