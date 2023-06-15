503836cb81b3530917e73e30ffac85aa96ee687c
davidstanciu11 <stanciudavid169@gmail.com>
2023-06-15 13:45:35 +0300

    new banner after deployment

--- src/components/Purchase.jsx
+++ src/components/Purchase.jsx
@@ -1,11 +1,10 @@
 import { Button } from "@mui/material";
 import "./Purchase.style.css";
-import AdvertismentBanner from "../assets/advertisment.jpg";
 
 export default function Purchase() {
   return (
     <>
-      <section className="purchase_section" style={{background: `url(${AdvertismentBanner})`}}>
+      <section className="purchase_section" style={{background: `url(https://d2kpe7grvhf8ri.cloudfront.net/website/optimized_images/theme-hero.jpg)`}}>
         <div className="purchase_flex">
           <h2 className="purchase_title">
             <span className="purchase_start">Start Now</span> <br /> Create Your <br /> Website Today
