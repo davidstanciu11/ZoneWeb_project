30ea5fc7af95a34b5749d9c810a292e7039c8933
davidstanciu11 <stanciudavid169@gmail.com>
2023-06-15 12:23:34 +0300

    new commit

--- src/components/Purchase.jsx
+++ src/components/Purchase.jsx
@@ -10,7 +10,7 @@ export default function Purchase() {
           <h2 className="purchase_title">
             <span className="purchase_start">Start Now</span> <br /> Create Your <br /> Website Today
           </h2>
-          <Button className="figma_button">Purchase Now</Button>
+          <Button className="figma_button">Purchase Nosasadasda</Button>
         </div>
       </section>
     </>
